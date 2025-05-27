5e17e4d2ea130bb50c5eca911a3382f84b04b684
Farjana Eti <etimourakkhieti@gmail.com>
2025-05-28 00:28:01 +0600

    linear search

--- Array/DAY-2/ifarray-sorted.cpp
+++ Array/DAY-2/ifarray-sorted.cpp
@@ -7,22 +7,30 @@ Note: An array A rotated by x positions results in an array B of the same length
  
 
 Example 1:
-
-Input: nums = [3,4,5,1,2]
+Input: nums = [3,4,5,1,2] 3>4 4>0 5>1 1>2 2>3
+nums[i] > nums[(i + 1) % n]
 Output: true
 Explanation: [1,2,3,4,5] is the original sorted array.
 You can rotate the array by x = 3 positions to begin on the element of value 3: [3,4,5,1,2].
-Example 2:
 
+Example 2:
 Input: nums = [2,1,3,4]
 Output: false
 Explanation: There is no sorted array once rotated that can make nums.
-Example 3:
 
+Example 3:
 Input: nums = [1,2,3]
 Output: true
 Explanation: [1,2,3] is the original sorted array.
 You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+
+ the array is "rotated," the next element after the last index n-1 is the first element nums[0].
+So (i + 1) % n wraps around the index to 0 after reaching the end.
+If i is at the last index (say i = n-1), then:
+
+(i + 1) % n = (n-1 + 1) % n = n % n = 0
+
+So nums[(i + 1) % n] = nums[0] → the first element of the array
 
 ********code********
 class Solution {
--- Array/DAY-2/second-largest.cpp
+++ Array/DAY-2/second-largest.cpp
@@ -3,20 +3,15 @@ Given an array of integers nums, return the second-largest element in the array.
 
 Examples:
 Input: nums = [8, 8, 7, 6, 5]
-
 Output: 7
-
 Explanation: The largest value in nums is 8, the second largest is 7
 
 Input: nums = [10, 10, 10, 10, 10]
-
 Output: -1
-
 Explanation: The only value in nums is 10, so there is no second largest value, thus -1 is returned
 
 *******solve 1*************
-int maxVal = nums[0];
-        
+   int maxVal = nums[0];   
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i] > maxVal) {
                 maxVal = nums[i];
